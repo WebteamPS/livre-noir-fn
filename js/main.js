10bc8d8053f65b4ba3ba89c5a6f6bfc174a40b98
WebteamPS <webmasterparti-socialiste.fr>
2015-12-09 14:36:31 +0100

    add progress bar and btn go to top

--- js/main.js
+++ js/main.js
@@ -7,3 +7,34 @@ $('a[href^="#"]').click(function(){
   }, 'slow');
   return false;
 });
+//bouton to top
+$(document).ready( function () {
+  // Add return on top button
+  $('body').append('<div id="go_top" title="Retour en haut"><i class="glyphicon glyphicon-chevron-up"></i></div>');
+  
+  // On button click, let's scroll up to top
+  $('#go_top').click( function() {
+      $('html,body').animate({scrollTop: 0}, 'slow');
+  });
+});
+
+$(window).scroll(function() {
+    // If on top fade the bouton out, else fade it in
+    if ( $(window).scrollTop() == 0 )
+        $('#go_top').fadeOut();
+    else
+        $('#go_top').fadeIn();
+});
+//progress bar reading
+var winHeight = $(window).height(), 
+  docHeight = $(document).height(),
+  progressBar = $('progress'),
+  max, value;
+/* Set the max scrollable area */
+max = docHeight - winHeight;
+progressBar.attr('max', max);
+
+$(document).on('scroll', function(){
+   value = $(window).scrollTop();
+   progressBar.attr('value', value);
+});
